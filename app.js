1e73fd1a97fbbf25d66d2ed5e5a86f53b136c750
panjasaurabh@gmail.com <panjasaurabh@gmail.com>
2018-08-22 20:52:22 +0530

    added app.js

--- app.js
+++ app.js
@@ -1,17 +1,85 @@
 var express    = require("express"),
+    app        = express(),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose");
 
-mongoose.connect("mongodb://localhost/blogApp");
+mongoose.connect("mongodb://localhost/blogApp",{ useNewUrlParser: true });
 
-app.use("view engine","ejs");
-app.set(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/",function (req,res) {
-  res.send("Hello");
+
+//Database Schema
+
+var blogSchema = new mongoose.Schema({
+  title : String,
+  image : String,
+  description : String,
+  created: {type:Date, default: Date.now}
 });
 
+var Blog = mongoose.model("blog",blogSchema);
+
+//Restful routes
+
+app.get("/",function (req,res) {
+  res.redirect("/blogs");
+});
+
+//index route
+app.get("/blogs",function (req,res) {
+  Blog.find({},function (err,data) {
+    if (err) {
+      console.log(err);
+    }else {
+      res.render("index",{blogs:data});
+    }
+  });
+});
+
+//show
+app.get("/blogs/show/:id",function (req,res) {
+  Blog.findById(req.params.id,function (err,data) {
+    if (err) {
+      console.log(err);
+    }else {
+      res.render("show",{blog:data});
+    }
+  });
+});
+//new
+app.get("/blogs/new",function (req,res) {
+  res.render("new");
+});
+//create route
+app.post("/blogs",function (req,res) {
+  Blog.create({
+    title : req.body.title,
+    description : req.body.description,
+    image: req.body.image
+  },function (err,data) {
+    if (err) {
+      console.log(err);
+    }else {
+      res.redirect("/");
+      console.log(data);
+    }
+  });
+});
+//update route
+app.get("/blogs",function (req,res) {
+  res.send("Hello");
+});
+//delete route
+app.get("/blogs",function (req,res) {
+  res.send("Hello");
+});
+//edit route
+app.get("/blogs/:id/edit",function (req,res) {
+  res.send("Hello");
+});
+//else if
 app.get("*",function (req,res) {
   res.send("Error 404");
 });
